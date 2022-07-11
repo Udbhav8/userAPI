65d0fe92b8ebb1f0784e879986db7d232273a7c2
Udbhav Agarwal <udbhav.agarwal7@gmail.com>
2022-07-11 22:53:45 +0530

    fixed envs

--- users_app/settings.py
+++ users_app/settings.py
@@ -13,7 +13,6 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 import sys
-import dj_database_url
 from django.core.management.utils import get_random_secret_key
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,12 +74,18 @@ WSGI_APPLICATION = 'users_app.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+import dj_database_url
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
+ 
 
 
 # Password validation
