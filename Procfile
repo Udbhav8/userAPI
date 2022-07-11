8e5df985c655259ae11d775ea1c209e7cb67055f
Udbhav Agarwal <udbhav.agarwal7@gmail.com>
2022-07-11 22:40:39 +0530

    more fixes

--- Procfile
+++
@@ -1,3 +0,0 @@
-web: gunicorn users_api.wsgi
-release: python manage.py makgemigrations --noinput
-release: python manage.py migrate --noinput
--- users_app/settings.py
+++ users_app/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
-
+import os
+import sys
+import dj_database_url
+from django.core.management.utils import get_random_secret_key
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -19,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g_xe-96fkr6c@trh2d&t^-c@g_jd5)y4wrs)#!s)f^q8e)xrv5'
-
+SECRET_KEY = os.getenv("SECRET_KEY", get_random_secret_key())
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
@@ -116,6 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
